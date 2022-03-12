0a5c87c5ae25d5621dafc45af6ffcefa7e3e75e5
OCR <kuchuk.sa17@gmail.com>
2022-03-12 14:51:30 +0300

    add thymeleaf

--- src/main/java/by/tms/config/WebConfiguration.java
+++ src/main/java/by/tms/config/WebConfiguration.java
@@ -1,29 +1,54 @@
 package by.tms.config;
 
 import by.tms.interceptor.TestInterceptor;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
 @ComponentScan("by.tms")
 @EnableWebMvc
-public class WebConfiguration extends WebMvcConfigurerAdapter {
+public class WebConfiguration extends WebMvcConfigurerAdapter implements ApplicationContextAware {
 
 	@Autowired
 	private TestInterceptor testInterceptor;
 
+	private ApplicationContext applicationContext;
+
 	@Bean
-	public ViewResolver viewResolver(){
-		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-		viewResolver.setSuffix(".jsp");
-		viewResolver.setPrefix("/pages/");
+	public SpringResourceTemplateResolver templateResolver(){
+		SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+		templateResolver.setApplicationContext(this.applicationContext);
+		templateResolver.setPrefix("/pages/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		templateResolver.setCacheable(false);
+		return templateResolver;
+	}
+
+	@Bean
+	public SpringTemplateEngine templateEngine(){
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		templateEngine.setEnableSpringELCompiler(true);
+		return templateEngine;
+	}
+
+	@Bean
+	public ThymeleafViewResolver viewResolver(){
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine());
 		return viewResolver;
 	}
 
@@ -32,5 +57,10 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 		registry.addInterceptor(testInterceptor)
 				.addPathPatterns("/calc/**");
 //				.excludePathPatterns("/user/reg");
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
 	}
 }
